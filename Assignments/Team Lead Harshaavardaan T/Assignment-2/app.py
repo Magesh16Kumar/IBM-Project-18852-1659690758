a90864e674e1318c0c339c4a16a9658e429c11fc
Harshaa Vardaan <harshaaloga@gmail.com>
2022-09-30 15:52:21 +0530

    Modify assignment-2 Harshaa

---
+++ Assignments/Team Lead Harshaavardaan T/Assignment-2/app.py
@@ -0,0 +1,22 @@
+from re import L
+from requests import post
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/')
+def get_method():
+    return 'You have invoked GET HTTP method'
+
+@app.put('/')
+def put_method():
+    return 'You have invoked PUT HTTP method'
+
+@app.delete('/')
+def delete_method():
+    return 'You have invoked DELETE HTTP method'
+
+@app.post('/')
+def post_method():
+    return 'You have invoked POST HTTP method'
+
